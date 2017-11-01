a66ab623ad9ce015b67b399b1c8fe220bd224c12
hou <houxd1992@163.com>
2017-11-01 13:47:16 +0800

    生成仓库第一次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <el-button>默认按钮</el-button>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+
+</style>
